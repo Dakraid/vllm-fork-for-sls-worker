9f25ec60d26810b5066d65edc7aeda38059d449d
alpayariyak <aariyak@wpi.edu>
2024-01-24 01:48:43 +0000

    Fix bug

--- vllm/engine/ray_utils.py
+++ vllm/engine/ray_utils.py
@@ -1,5 +1,5 @@
 from typing import Optional, List, Tuple, TYPE_CHECKING
-
+import os
 from vllm.config import ParallelConfig
 from vllm.logger import init_logger
 from vllm.utils import is_hip, set_cuda_visible_devices, get_ip
